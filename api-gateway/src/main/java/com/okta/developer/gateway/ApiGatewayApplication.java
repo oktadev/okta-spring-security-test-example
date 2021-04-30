8e28736b7e87bee55aa19ea59a528b9ab28be585
indiepopart <jimena@gmail.com>
2021-04-30 01:49:49 -0300

    update cloud version to remove deprecated tokenrelayfilter

--- api-gateway/src/main/java/com/okta/developer/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/okta/developer/gateway/ApiGatewayApplication.java
@@ -1,21 +1,25 @@
 package com.okta.developer.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory filterFactory) {
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 			.route("listing", r -> r.path("/listing/**")
 				.filters(f -> f.filter(filterFactory.apply()))
